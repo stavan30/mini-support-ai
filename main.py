79c23e624ae834f7a056cd9cacecd9633b826f60
stavan30 <stavanpatel107@gmail.com>
2025-06-04 23:35:11 -0400

    Step 2: Clean and preprocess data

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from src.load_data import load_high_quality_questions
+from src.clean_data import clean_dataset
 
 if __name__ == "__main__":
     data_path = "data/train.csv"
+    
+    #Step-1: Load Data and load HQ questions (src/load_data.py)
     hq_df = load_high_quality_questions(data_path)
 
-    print("\n--- Sample Rows ---")
-    print(hq_df.head())
+    #Step-2: Clean Data
+    clean_df = clean_dataset(hq_df)
+
+    print("\n--- Cleaned Rows ---")
+    print(clean_df.head())
